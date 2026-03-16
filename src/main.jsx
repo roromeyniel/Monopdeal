1a1ee2fc9bd61d768f700251aff2c8cc56558b99
roromeyniel <rorogaming04@gmail.com>
2026-03-16 17:58:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 style={{color:'red'}}>Test</h1>
 );
